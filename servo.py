f936b515a8234d801d61776dd3eadd7635f0d717
Devanshi Padsala <devanshipadsala@gmail.com>
2018-03-07 20:43:14 -0600

    Add files via upload

--- servo.py
+++ servo.py
@@ -1,16 +1,24 @@
 import RPi.GPIO as GPIO
+from time import sleep
 GPIO.setmode(GPIO.BOARD)
 servoPin=11
 GPIO.setup(servoPin, GPIO.OUT)
 pwm=GPIO.PWM(servoPin, 50)
 pwm.start(7)
-for i in range(0,180):
-	DC=1./18.*(i)+2
-	pwm.ChangeDutyCycle(DC)
-	time.sleep(.05)
-for i in range(180,0,-1):
-	DC=1/18.*i+2
-	pwm.ChangeDutyCycle(DC)
-	time.sleep(.05)
+
+def lock():
+	for i in range(0,180):
+		DC=1./18.*(i)+2
+		pwm.ChangeDutyCycle(DC)
+		time.sleep(.05)
+	return True
+
+def lock_open():
+	for i in range(180,0,-1):
+		DC=1/18.*i+2
+		pwm.ChangeDutyCycle(DC)
+		time.sleep(.05)
+	return True
+
 pwm.stop()
 GPIO.cleanup()
