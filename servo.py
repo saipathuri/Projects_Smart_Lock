b820a99b51065dfb30c9f831eb3b3fad28c0d4e9
TaahaKamal5 <taahakamal#~#TaahaKamal5%^>
2018-03-21 21:27:01 -0500

    bug fixes

--- servo.py
+++ servo.py
@@ -1,23 +1,20 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
-servoPin=14
+servoPin=8
 GPIO.setup(servoPin, GPIO.OUT)
 pwm=GPIO.PWM(servoPin, 50)
 pwm.start(7)
 
 def lock():
-	for i in range(0,180):
-		DC=1./18.*(i)+2
-		pwm.ChangeDutyCycle(DC)
-		time.sleep(.05)
+	DC=1./18.*(180)+2
+	pwm.ChangeDutyCycle(DC)
 	return True
 
-def lock_open():
-	for i in range(180,0,-1):
-		DC=1/18.*i+2
-		pwm.ChangeDutyCycle(DC)
-		time.sleep(.05)
+def unlock():
+	DC=1/18.*(90)+2
+	pwm.ChangeDutyCycle(DC)
+	sleep(.05)
 	return True
 
 def clean_up():
