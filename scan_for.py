7ba044b79ef4c999947d01752a72e8c4614e2712
oliverwarne <oliverswarne@gmail.com>
2018-04-01 22:01:16 -0500

    ruh roh

--- scan_for.py
+++ scan_for.py
@@ -1,5 +1,7 @@
 import find_macs
-def main():
-    for i in find_macs.mac_addresses():
-        print(str(i))
+import savedData
 
+def main():
+    savedData.    
+    for i in find_macs.mac_addresses():
+        
